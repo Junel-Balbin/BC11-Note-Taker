2738a86f4ae183ded8516269b0f72673bc2f27d0
Junel <robin.lennuj@gmail.com>
2023-08-17 13:17:04 -0700

    Working on the routes.

---
+++ routes/notes.js
@@ -0,0 +1,4 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend, writeToFile, } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+const fs = require('fs');
